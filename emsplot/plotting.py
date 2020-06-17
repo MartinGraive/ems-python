17aff581f5df50b923f40860a7a6efd1bbd235d4
Martin Graive <martin.graive@eleves.enpc.fr>
2020-06-18 08:44:14 +1000

    Fix when time not specified in plot_ts

--- emsplot/plotting.py
+++ emsplot/plotting.py
@@ -350,7 +350,7 @@ def plot_ts(data, varname, indexes, ax=None, latlon=None, **kwargs):
     time_dim = [dim for dim in var.dims if dim not in lat.dims][0]
     for k, i in (data.coords.items() if len(data.coords) > 0 else data.items()):
         if len(i.dims) == 1 and i.dims[0] == time_dim:  # fixme: hack
-            j = i[indexes[time_dim]]
+            j = i[indexes[time_dim]] if time_dim in indexes else i
             try:
                 labels = np.datetime_as_string(j, 'auto')
             except TypeError:
